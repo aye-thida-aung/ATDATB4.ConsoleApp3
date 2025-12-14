250267b90e4fa5096319e2e34f071b05e1e58485
aye-thida-aung <ayethidaraung2213@gmail.com>
2025-12-14 21:31:18 +0630

    feat ado.net and dapper

---
+++ ATDATB4.ConsoleApp3/AdoDotNetSample.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB4.ConsoleApp3;
+
+internal class AdoDotNetSample
+{
+    private SqlConnectionStringBuilder sb;
+    //private SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder
+    //{
+    //    DataSource = ".",
+    //    InitialCatalog = "Batch4MiniPOS",
+    //    UserID = "sa",
+    //    Password = "sasa@123",
+    //    TrustServerCertificate = true
+    //};
+
+    public AdoDotNetSample()
+    {
+        sb = new SqlConnectionStringBuilder();
+        sb.DataSource = "."; // server name
+        sb.InitialCatalog = "Batch4MiniPOS"; // database name
+        sb.UserID = "sa";
+        sb.Password = "12345";
+        sb.TrustServerCertificate = true;
+    }
+
+    //public AdoDotNetSample(SqlConnectionStringBuilder sb)
+    //{
+    //    this.sb = sb;
+    //}
+
+    public void Read()
+    {
+        SqlConnection connection = new SqlConnection(sb.ConnectionString);
+
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand("select * from tbl_product", connection);
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adapter.Fill(dt); // execute
+
+        connection.Close();
+
+        // table > row > column
+        foreach (DataRow dr in dt.Rows)
+        {
+            int porductId = Convert.ToInt32(dr["ProductId"]);
+            string productName = Convert.ToString(dr["ProductName"]);
+            decimal price = Convert.ToDecimal(dr["Price"]);
+            Console.WriteLine("Product ID : " + porductId);
+
+            Console.WriteLine("Product Name : " + productName);
+            Console.WriteLine($"Price : {price.ToString("n0")}");
+        }
+    }
+
+    public void Edit()
+    {
+        SqlConnection connection = new SqlConnection(sb.ConnectionString);
+
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand("select * from tbl_product where productid = 1", connection);
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adapter.Fill(dt); // execute
+
+        connection.Close();
+
+        // 1 or 0 - success / fail
+
+        if (dt.Rows.Count == 0) // > 0 ?
+            return;
+
+        DataRow dr = dt.Rows[0];
+
+        string productName = Convert.ToString(dr["ProductName"]);
+        decimal price = Convert.ToDecimal(dr["Price"]);
+        Console.WriteLine("Product Name : " + productName);
+        Console.WriteLine($"Price : {price.ToString("n0")}");
+    }
+
+    public void Create()
+    {
+        SqlConnection connection = new SqlConnection(sb.ConnectionString);
+
+        connection.Open();
+
+        string query = @"INSERT INTO [dbo].[Tbl_Product]
+           ([ProductName]
+           ,[Price]
+           ,[Quantitty]
+           ,[IsDelete]
+           ,[CreatedDateTime])
+     VALUES
+           ('Pineapple'
+           ,4000
+           ,30
+           ,0
+           ,GETDATE())";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+        Console.WriteLine(message);
+    }
+
+    public void Update()
+    {
+        SqlConnection connection = new SqlConnection(sb.ConnectionString);
+
+        connection.Open();
+
+        string query = @"UPDATE Tbl_Product
+                     SET ProductName = 'Updated Apple Apple',
+                         Price = 5000
+                     WHERE ProductId = 1";
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        string message = result > 0 ? "Update Successful." : "Update Failed.";
+        Console.WriteLine(message);
+    }
+
+
+    public void Delete()
+    {
+        SqlConnection connection = new SqlConnection(sb.ConnectionString);
+
+        connection.Open();
+
+        string query = @"UPDATE Tbl_Product
+                     SET IsDelete = 1
+                     WHERE ProductId = 6";
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        string message = result > 0 ? "Delete Successful." : "Delete Failed.";
+        Console.WriteLine(message);
+    }
+
+}
---
+++ ATDATB4.ConsoleApp3/DapperSample.cs
@@ -0,0 +1,126 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATDATB4.ConsoleApp3;
+
+
+internal class DapperSample
+{
+    private SqlConnectionStringBuilder sb;
+
+    public DapperSample()
+    {
+        sb = new SqlConnectionStringBuilder();
+        sb.DataSource = "."; // server name
+        sb.InitialCatalog = "Batch4MiniPOS"; // database name
+        sb.UserID = "sa";
+        sb.Password = "12345";
+        sb.TrustServerCertificate = true;
+    }
+
+    public void Read()
+    {
+        using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+        {
+            db.Open();
+            string query = "SELECT * FROM Tbl_Product WHERE IsDelete = 0";
+
+            List<ProductDto> lst = db.Query<ProductDto>(query).ToList();
+
+            foreach (ProductDto item in lst)
+            {
+                Console.WriteLine(item.ProductId);
+                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.Price);
+                Console.WriteLine(item.Quantity);
+            }
+        }
+    }
+
+    public void Edit()
+    {
+        using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+        {
+            db.Open();
+            string query = "select * from tbl_product where productid = 1;";
+            ProductDto item = db.Query<ProductDto>(query).FirstOrDefault()!;
+
+            //if (item == null) return;
+            if (item is null) return;
+
+            Console.WriteLine(item.ProductId);
+            Console.WriteLine(item.ProductName);
+            Console.WriteLine(item.Price);
+            Console.WriteLine(item.Quantity);
+        }
+    }
+
+    public void Create()
+    {
+        using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+        {
+            db.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Product]
+           ([ProductName]
+           ,[Price]
+           ,[Quantitty]
+           ,[IsDelete]
+           ,[CreatedDateTime])
+     VALUES
+           ('Pineapple'
+           ,4000
+           ,30
+           ,0
+           ,GETDATE())";
+            int result = db.Execute(query);
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+    }
+
+    public void Update()
+    {
+        using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+        {
+            db.Open();
+
+            string query = @"UPDATE Tbl_Product
+                         SET ProductName = 'Banana',
+                             Price = 500,
+                             Quantitty = 20
+                         WHERE ProductId = 6";
+
+            int result = db.Execute(query);
+
+            string message = result > 0 ? "Update Successful." : "Update Failed.";
+            Console.WriteLine(message);
+        }
+    }
+
+
+    public void Delete()
+    {
+        using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+        {
+            db.Open();
+
+            string query = @"UPDATE Tbl_Product
+                         SET IsDelete = 1
+                         WHERE ProductId = 6";
+
+            int result = db.Execute(query);
+
+            string message = result > 0 ? "Delete Successful." : "Delete Failed.";
+            Console.WriteLine(message);
+        }
+    }
+
+}
